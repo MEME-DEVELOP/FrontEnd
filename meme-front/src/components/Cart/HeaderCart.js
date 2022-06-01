eabc242be005b7b676fac4cc3803f321e8e355e8
byepesg <byepesg@unal.edu.co>
2022-05-31 20:56:50 -0500

    Commit 1 No revisado

--- meme-front/src/components/Cart/HeaderCart.js
+++ meme-front/src/components/Cart/HeaderCart.js
@@ -1,15 +1,23 @@
 import React from 'react'
 
-export default function HeaderCart(props){
-    return(
 
-        <HeaderCart className="row block center ">
-            <div>
-                <a href="#/">
-                    <h1>Small Shopping Cart</h1> 
-                </a>
-            </div>
-        </HeaderCart>
-    )
+export default function HeaderCart(props){
+ 
+        return(
+
+ 
+            <header className ="row block center">
+                <div>                    
+                        <h1>
+                           Small Shopping Cart 
+                        </h1>                    
+                </div>
+                <div>
+                    <h1>Cart</h1>
+                </div>
+            </header>
+        )
+    
+    
 
 }
