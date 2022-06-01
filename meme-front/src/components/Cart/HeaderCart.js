bb0220579e6c9b2cdb11f85b7c0db053245c7964
byepesg <byepesg@unal.edu.co>
2022-05-31 22:55:31 -0500

    Carrito de compras V2

--- meme-front/src/components/Cart/HeaderCart.js
+++ meme-front/src/components/Cart/HeaderCart.js
@@ -6,15 +6,37 @@ export default function HeaderCart(props){
         return(
 
  
-            <header className ="row block center">
-                <div>                    
-                        <h1>
-                           Small Shopping Cart 
-                        </h1>                    
-                </div>
-                <div>
-                    <h1>Cart</h1>
-                </div>
+            <header className ="row block2 center">
+         <div>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+          <h1>Carrito de compras MEME</h1>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+        
+      </div>
+      <div>
+        
+          <h1>Elementos{' '}</h1>
+          <br></br>
+            
+            
+          {props.countCartItems ? (
+            <button className="badge">{props.countCartItems}</button>
+          ) : (
+            ''
+          )}
+          <br></br>
+            <br></br>
+            <br></br>
+        {' '}
+        
+      </div>
+      
             </header>
         )
     
--- meme-front/src/components/Cart/MainCart.js
+++ meme-front/src/components/Cart/MainCart.js
@@ -2,16 +2,16 @@ import React from 'react'
 import Product from '../Product';
 
 export default function MainCart(props){
-    const {products} = props;
+    const {products,onAdd} = props;
     return(
 
-        <main className="block col-2">
-        <h2>Products</h2>
-        <div className="row">
-          {products.map((product) => (
-            <Product key={product.id} product={product}></Product>
-          ))}
-        </div>
+        <main className="row">
+ <h2>Products</h2>
+      <div className="row">
+        {products.map((product) => (
+          <Product key={product.id} product={product} onAdd={onAdd}></Product>
+        ))}
+      </div>
       </main>
     )
 
